d83f470a4b72d52b19f452bbb4c0cdca270970b4
fangqing <fangqing@114study.com>
2017-04-14 11:07:46 +0800

    update

---
+++ src/components/public/bt_back.vue
@@ -0,0 +1,12 @@
+<template>
+	 <Button type="primary" @click="history">返回</Button>
+</template>
+<script type="text/javascript">
+	export default {
+		methods : {
+			history : function(){
+				window.history.back();
+			}
+		}
+	}
+</script>
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -5,7 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: {
         main: './src/main',
-        vendors: './src/config/vendors'
+        // vendors: './src/config/vendors'
     },
     output: {
         path: path.join(__dirname, './dist')
